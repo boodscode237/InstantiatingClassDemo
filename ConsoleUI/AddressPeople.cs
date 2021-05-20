9492cb5d37d8de4dd30d2396a83ea75f7e16b93e
BriceDon <65740750+boodscode237@users.noreply.github.com>
2021-05-21 01:51:36 +0500

    Add project files.

---
+++ ConsoleUI/AddressPeople.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class AddressPeople
+    {
+        public static void GiveAddress(PersonModel person)
+        {
+            Console.WriteLine($"firstName : {person.FirstName} \nlastname : {person.LastName}\nemail address: {person.EmailAddress}");
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            string firstName = "";
+
+            do
+            {
+                Console.Write("What is your first name {or type exit to exit}: ");
+                firstName = Console.ReadLine();
+
+                Console.Write("What is your last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("What is your email? ");
+                string email = Console.ReadLine();
+
+                if (firstName.ToLower() != "exit")
+                {
+                    PersonModel person = new PersonModel();
+                    person.FirstName = firstName;
+                    person.LastName = lastName;
+                    person.EmailAddress = email;
+                    people.Add(person);
+                }
+            } while (firstName.ToLower() != "exit");
+
+            foreach (PersonModel p in people)
+            {
+                ProcessPerson.GreetPerson(p);
+                AddressPeople.GiveAddress(p);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
